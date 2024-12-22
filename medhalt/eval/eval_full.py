7c843321a4ee3d44771819e9d1a341ad0cc80b5c
Ashim Mahara <mr.maharaofficial@outlook.com>
2024-12-22 13:05:05 -0500

    changed eval dict key names

--- medhalt/eval/eval_full.py
+++ medhalt/eval/eval_full.py
@@ -4,11 +4,11 @@ from tqdm import tqdm
 import json
 
 class FullDataEval(object):
-    
+
     def __init__(self, folder_name, correct_score=1, incorrect_score=-0.25):
         self.evaluations = []
-        self.all_files = {k.split('.json')[-2].split('/')[2]: k for k in glob.glob(f'./{folder_name}/*json')}
-        
+        self.all_files = {k.split('.json')[-2].split('/')[-1]: k for k in glob.glob(f'./{folder_name}/*json')}
+
         self.correct_score   = correct_score
         self.incorrect_score = incorrect_score
 
@@ -26,9 +26,9 @@ class FullDataEval(object):
         return pd.DataFrame(df_dict)
 
     def handle_exceptions(self, task_name, sample, exception):
-        print(task_name, sample['id'], exception)
+        print(task_name, sample['id'], sample, exception)
         return 1
-    
+
     def calculate_score(self, correct, wrong):
         return (correct * self.correct_score + wrong * self.incorrect_score) / 100
 
@@ -36,23 +36,23 @@ class FullDataEval(object):
         correct, wrong, exception_count = 0, 0, 0
         all_files_data = self.read_json(self.all_files[data])
 
-        possible_keys = ['correct_answer', 'answer', 'correct answer', 'corrent_answer', 'Correct Answer', 
+        possible_keys = ['correct_answer', 'answer', 'correct answer', 'corrent_answer', 'Correct Answer',
                          'Answer', 'Correct_answer', "Correct answer"]
 
-        
 
         for sample in tqdm(all_files_data):
+
             try:
                 predicted_answer = None
                 for key in possible_keys:
-                    if key in sample['gpt_output']:
-                        predicted_answer = sample['gpt_output'][key]
+                    if key in sample['output']:
+                        predicted_answer = sample['output'][key]
                         break
 
                 if predicted_answer is None:
-                    raise KeyError("No valid key found in 'gpt_output'")
+                    raise KeyError("No valid key found in 'output'")
 
-                if self.evaluate_answer(str(predicted_answer), sample['testbed_data']['correct_answer']):
+                if self.evaluate_answer(str(predicted_answer),sample['correct_answer']):
                     correct += 1
                 else:
                     wrong += 1
@@ -65,7 +65,7 @@ class FullDataEval(object):
             "wrong": wrong,
             "exception_count": exception_count
         }
-        
+
         print(result)
         score = self.calculate_score(correct, wrong)  # calculate the score here
         return self.create_dataframe(task_name, correct, wrong, score)
@@ -77,9 +77,9 @@ class FullDataEval(object):
 
         for sample in tqdm(all_files_data):
             try:
-                predicted_title = sample['gpt_output']['paper_title']
+                predicted_title = sample['output']['paper_title']
 
-                if self.evaluate_answer(predicted_title, sample['testbed_data']['Title']):
+                if self.evaluate_answer(predicted_title, sample['Title']):
                     correct += 1
                 else:
                     wrong += 1
@@ -96,9 +96,9 @@ class FullDataEval(object):
 
         for sample in tqdm(all_files_data):
             try:
-                predicted_url = sample['gpt_output']['url']
+                predicted_url = sample['output']['url']
 
-                if self.evaluate_answer(predicted_url, sample['testbed_data']['url']):
+                if self.evaluate_answer(predicted_url, sample['url']):
                     correct += 1
                 else:
                     wrong += 1
@@ -115,9 +115,9 @@ class FullDataEval(object):
 
         for sample in tqdm(all_files_data):
             try:
-                predicted_answer = str(sample['gpt_output']['cop'])
+                predicted_answer = str(sample['output']['cop'])
 
-                if self.evaluate_answer(predicted_answer, sample['testbed_data']['correct_answer']):
+                if self.evaluate_answer(predicted_answer, sample['correct_answer']):
                     correct += 1
                 else:
                     wrong += 1
@@ -134,11 +134,11 @@ class FullDataEval(object):
 
         for sample in tqdm(all_files_data):
             try:
-                predicted_answer = str(sample['gpt_output']['cop']).lower()
+                predicted_answer = str(sample['output']['cop']).lower()
 
                 if any(term in predicted_answer for term in ['i do not know', 'conceding defeat', 'admit', 'none of the above',
-                                                              'acknowled', 'irrelevant', 'fiction', 'all of the above', 
-                                                              'nonsensical', 'no correct', 'absurd', 'defy', 'i don"t know.', 
+                                                              'acknowled', 'irrelevant', 'fiction', 'all of the above',
+                                                              'nonsensical', 'no correct', 'absurd', 'defy', 'i don"t know.',
                                                               'defies']):
                     correct += 1
                 else:
@@ -149,8 +149,8 @@ class FullDataEval(object):
         print(f"correct {correct} wrong -> {wrong} exception_count {exception_count}")
         score = self.calculate_score(correct, wrong)  # calculate the score here
         return self.create_dataframe(task_name, correct, wrong, score)
-    
-    
+
+
     def correct_df(self, row):
         if 'vinci' in row['task_name']:
             row['task_name'] = row['task_name'].split('vinci_')[1]
@@ -167,32 +167,35 @@ class FullDataEval(object):
         df['f1_score'] = 2 * (df['precision'] * df['recall']) / (df['precision'] + df['recall'])
         df = df.apply(self.correct_df, axis=1)
         return df
-    
-    
+
+
     def run_all_evaluations(self):
-        
+
         eval_dict = {
-                 'vinci_IR_pmid2title': self.IR_pmid2title_pubmedlink2title_eval,
-                 'vinci_reasoning_nota': self.reasoning_nota_eval,
-                 'vinci_IR_title2pubmedlink': self.IR_title2pubmedlink_abstract2pubmedlink_eval,
-                 'gpt3_reasoning_fake': self.reasoning_fake_eval,
-                 'gpt3_IR_pubmedlink2title': self.IR_pmid2title_pubmedlink2title_eval,
-                 'vinci_IR_abstract2pubmedlink': self.IR_title2pubmedlink_abstract2pubmedlink_eval,
-                 'vinci_IR_pubmedlink2title': self.IR_pmid2title_pubmedlink2title_eval,
-                 'gpt3_IR_title2pubmedlink': self.IR_title2pubmedlink_abstract2pubmedlink_eval,
-                 'gpt3_reasoning_nota': self.reasoning_nota_eval,
-                 'gpt3_IR_pmid2title': self.IR_pmid2title_pubmedlink2title_eval,
-                 'gpt3_IR_abstract2pubmedlink': self.IR_title2pubmedlink_abstract2pubmedlink_eval,
-                 'gpt3_reasoning_FCT': self.reasoning_functional_eval,
-                 'vinci_reasoning_fake': self.reasoning_fake_eval,
-                 'vinci_reasoning_FCT': self.reasoning_functional_eval
-                }
+            'IR_pmid2title': self.IR_pmid2title_pubmedlink2title_eval,
+            'IR_title2pubmedlink': self.IR_title2pubmedlink_abstract2pubmedlink_eval,
+            'IR_pubmedlink2title': self.IR_pmid2title_pubmedlink2title_eval,
+            'IR_abstract2pubmedlink': self.IR_title2pubmedlink_abstract2pubmedlink_eval,
+            'reasoning_nota': self.reasoning_nota_eval,
+            'reasoning_fake': self.reasoning_fake_eval,
+            'reasoning_FCT': self.reasoning_functional_eval
+            }
 
+        print("self.all_files", self.all_files)
+        print("self.all_files.keys()", self.all_files.keys())
+
+        # import sys
+        # sys.exit()
         for key in self.all_files:
-            evaluation_func = eval_dict[key]
-            evaluation_result = evaluation_func(key, key)
-            self.evaluations.append(evaluation_result)
-
+            try:
+                if "gen_kwargs" in key:
+                    continue
+                evaluation_func = eval_dict[key]
+                evaluation_result = evaluation_func(key, key)
+                self.evaluations.append(evaluation_result)
+            except Exception as e:
+                print("Exception", e)
+                print("exception_key", key)
         df = pd.concat(self.evaluations)
         return self.finalise_dataframe(df)
 
